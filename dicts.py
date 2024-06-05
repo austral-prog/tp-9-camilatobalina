4ec66e818390299bc79992ea1c7420b594914cf7
Camila Tobalina <162723051+camilatobalina@users.noreply.github.com>
2024-06-05 15:59:26 -0300

    Update dicts.py

--- dicts.py
+++ dicts.py
@@ -36,5 +36,5 @@ def list_inventory(inventory):
     list= []
     for item in inventory.items():
         if item[1]>0:
-            list+=item
-            return list
+            list+=[item]
+    return list
